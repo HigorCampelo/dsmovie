983efb397314f34f8db1db3667488a0d466f0f72
Higor Campelo <higorxadrez@hotmail.com>
2022-03-13 02:09:11 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as StarFull } from 'assets/img/fullstar.svg';
+import { ReactComponent as StarHalf } from 'assets/img/halfstar.svg';
+import { ReactComponent as StarEmpty } from 'assets/img/emptystar.svg';
+import './styles.css';
+
+
+function MovieStars() {
+
+
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+        </div>
+    );
+}
+
+export default MovieStars;
